673f99d160da4373e81d2bb9ad9b0a9e70a11d77
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-15 19:51:34 +0200

    added hard datasets

--- infoGain.py
+++ infoGain.py
@@ -8,3 +8,30 @@ WHAT WE NEED:
 - Dataset
 - A way to input/read data
 '''
+# yes, no
+from importlib.machinery import WindowsRegistryFinder
+
+
+play = [9, 5]
+
+outlook = [
+    [4, 0], # overcast
+    [3, 2], # rainy
+    [2, 3]  # sunny
+]
+
+humidity = [
+    [3, 4], # high
+    [6, 1]  # normal
+]
+
+temperature = [
+    [3, 1], # cool
+    [2, 2], # mild
+    [4, 2]  # hot
+]
+
+windy = [
+    [6, 2], # false
+    [3, 3]  # true
+]
