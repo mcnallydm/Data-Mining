b2cadb2cdb210edcb871b70b41ae17efd6913ea2
Dara McNally (267982) <s-dm267982@acgadmin.edu>
2022-03-15 19:22:23 +0200

    Added base stuff

--- classExample.py
+++ classExample.py
@@ -1,3 +1,5 @@
 from optparse import OptionParser
 opts = OptionParser()
-opts.add_option()
+opts.add_option("--parameters", "-d", help = "...")   # python classExample.py -d 1 2 3
+options, args = opts.parse_args()
+print(options, "\n", args)
---
+++ infoGain.py
@@ -0,0 +1,10 @@
+'''
+WHAT WE NEED:
+- Probability of each outcome
+- Value of each outcome
+- Total # of instances
+- 2+ Groups
+- Entropy formula
+- Dataset
+- A way to input/read data
+'''
